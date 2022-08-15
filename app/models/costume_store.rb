f1a311d9c4763d41286d5dba4f3984585190877a
William Luck <luck.william.h@gmail.com>
2022-08-15 17:22:25 -0500

    commit tree 9dada86c160d60a8a15649fe5c92fb7f5961b72a

--- app/models/costume_store.rb
+++ app/models/costume_store.rb
@@ -1,1 +1,9 @@
 # Create your CostumeStore class here
+# Models inherit from ActiveRecord::Base, while migrations inherit from ActiveRecord::Migration. The latter usually has a method called change, which describes what the migration is doing. (Remember t as a block parameter)
+
+# When creating migrations, the file name in camel_case must match the class name in SnakeCase. 
+
+# The models don't have to have the defined methods we used in the past, because they are associated with the tables created in the migration. I think this is just active record migrations?
+
+class CostumeStore < ActiveRecord::Base
+end
